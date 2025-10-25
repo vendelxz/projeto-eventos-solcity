1b56af346f8e6bbe8ffec9595dae493b8cde4e8f
Wendel <jwend3l@gmail.com>
2025-10-24 21:24:06 -0300

    feat(organizadora): Entidade ORGANIZADORA e seus DTOS criados

---
+++ src/main/java/com/project/eventos/dtos/OrganizadoraDTO.java
@@ -0,0 +1,13 @@
+package com.project.eventos.dtos;
+
+import lombok.Data;
+
+@Data
+public class OrganizadoraDTO {
+    private Long id;
+    private Long ownerUsuarioId; // Retornamos apenas o ID do dono
+    private String displayName;
+    private Boolean verified;
+    private String contato;
+    private String address;
+}
