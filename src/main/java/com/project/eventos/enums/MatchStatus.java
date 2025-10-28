4dee6b3ac6b90c22952eea20800c2ac71402e05d
Wendel <jwend3l@gmail.com>
2025-10-28 19:30:58 -0300

    feat: Entidades, Match, Competition e Standing adicionadas com seus respectivos ENUMs

---
+++ src/main/java/com/project/eventos/enums/MatchStatus.java
@@ -0,0 +1,25 @@
+package com.project.eventos.enums;
+
+public enum MatchStatus {
+
+    //Agendada
+    SCHEDULED,
+
+    //Em andamento
+    IN_PROGRESS,
+
+    //Finalizada
+    FINISHED,
+
+    //Cancelada
+    CANCELED,
+
+    //Adiada
+    POSTPONED,
+
+    //Suspensa
+    SUSPENDED,
+
+    //Encerrada por W.O.
+    WALKOVER
+}
