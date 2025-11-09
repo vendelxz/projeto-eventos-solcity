41cd75059c23b6d60c385dc639eede40e8375700
Wendel <jwend3l@gmail.com>
2025-11-08 21:28:28 -0300

    feat(DTOs): RegisterUserDTO, LoginUserDTO e UserDTO criados

---
+++ src/main/java/com/project/eventos/dtos/LoginUserDTO.java
@@ -0,0 +1,16 @@
+package com.project.eventos.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginUserDTO {
+
+    @NotBlank(message = "O email é obrigatório.")
+    @Email(message = "Formato de email inválido.")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória.")
+    private String senha;
+}
